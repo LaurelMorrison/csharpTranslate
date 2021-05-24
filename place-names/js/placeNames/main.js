8cf1268bff7c92e473f9431f8f954de108287381
Laurel <lbentleym1992@gmail.com>
2021-05-24 13:20:28 -0400

    filtering place names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,13 @@
 // Put your code here
+
+const names = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log(names)
+
+function filterItems(arr, query) {
+    return arr.filter(function (el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+console.log(filterItems(names, 'the'))
