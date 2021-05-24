56954973892a397575353f463766ee9f076f01a4
Laurel <lbentleym1992@gmail.com>
2021-05-24 14:27:26 -0400

    enemy list translated

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,57 @@
 // Put your code here
+
+const getEnemies = () => {
+    const enemies =
+        [
+            {
+                FirstName: "Joshua",
+                LastName: "Flowers",
+                Offenses: [
+                    "Being a jerk to me in elementary school",
+                    "Not being nice to me in elementary school"
+                ],
+                IsReallyHated: true
+            },
+            {
+                FirstName: "Darth",
+                LastName: "Vader",
+                Offenses: [
+                    "Cut off Luke's hand",
+                    "Murdered all those kids",
+                    "Unkind management practices"
+                ],
+                IsReallyHated: false
+            },
+            {
+                FirstName: "Betty",
+                LastName: "Rudelady",
+                Offenses: [
+                    "Phone calls in the theater",
+                    "Phone calls on the bus",
+                    "Phone calls in line at the grocery store",
+                    "Poor conversationalist"
+                ],
+                IsReallyHated: true
+            },
+            {
+                FirstName: "Leon",
+                LastName: "Peck",
+                Offenses: [
+                    "Keeps giving me a hotplate"
+                ],
+                IsReallyHated: false
+            }
+        ]
+
+    return enemies;
+};
+
+const enemies = getEnemies();
+
+for (enemy of enemies) {
+    if (enemy.IsReallyHated === true) {
+        console.log(`${enemy.FirstName} ${enemy.LastName} ("Really, really dislike!")`);
+    } else {
+        console.log(`${enemy.FirstName} ${enemy.LastName} ("Just mild dislike")`);
+    }
+}
