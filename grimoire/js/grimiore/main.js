7ed5ac14e03266d0334f8b8a562288d26ef95bac
Laurel <lbentleym1992@gmail.com>
2021-05-24 16:50:41 -0400

    adding magic translation

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,54 @@
 // Put your code here
+
+const spells =
+    [
+        {
+            Name: "Turn enemy into a newt",
+            IsEvil: true,
+            EnergyReqired: 5.1
+        },
+        {
+            Name: "Conjure some gold for a local charity",
+            IsEvil: false,
+            EnergyReqired: 2.99
+        },
+        {
+            Name: "Give a deaf person the ability to heal",
+            IsEvil: false,
+            EnergyReqired: 12.2
+        },
+        {
+            Name: "Make yourself emperor of the universe",
+            IsEvil: true,
+            EnergyReqired: 100.0
+        },
+        {
+            Name: "Convince your relatives your political views are correct",
+            IsEvil: false,
+            EnergyReqired: 2921.5
+        }
+    ]
+
+
+const getGoodBooks = () => {
+
+    for (let spell of spells) {
+        if (spell.IsEvil === false) {
+
+            console.log(`${spell.Name}`)
+        }
+
+    };
+}
+
+const getEvilBooks = () => {
+    for (let spell of spells) {
+        if (spell.IsEvil === true) {
+            console.log(`${spell.Name}`)
+        }
+    }
+}
+
+console.log("Good Book"); getGoodBooks();
+console.log("Bad Book"); getEvilBooks();
+
