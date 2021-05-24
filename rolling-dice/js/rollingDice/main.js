7b94af1af334c3dc69109eddaf78dc7fed85b5eb
Laurel <lbentleym1992@gmail.com>
2021-05-24 15:25:47 -0400

    add die roll

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+function getRoll() {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+let dieRoll = () => {
+    for (let i = 0; i < 10; i++) {
+        let die1 = getRoll()
+        let die2 = getRoll()
+        let dieSum = die1 + die2
+        if (die1 === die2) {
+            console.log(`${die1} + ${die2} == ${dieSum} DOUBLES!`)
+        }
+        else {
+            console.log(`${die1} + ${die2} == ${dieSum}`)
+        };
+    }
+}
+dieRoll();
