8c0aea3a0fadf4093056177d675ef0f0e6172033
Laurel <lbentleym1992@gmail.com>
2021-06-04 16:07:22 -0400

    dinner party

---
+++ dinner-party/csharp/Guest.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace dinnerparty
+{
+    public class Guest
+    {
+        public string Name { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string Bio { get; set; }
+
+        public Guest(string name, string occupation, string bio)
+        {
+            Name = name;
+            Occupation = occupation;
+            Bio = bio;
+
+        }
+    }
+}
